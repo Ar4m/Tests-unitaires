f93cb224453a8b8283b63df2e62aa56f5b886433
David <davidelbar2@gmail.com>
2021-04-29 10:13:27 +0200

    ok

--- src/components/music-create.js
+++ src/components/music-create.js
@@ -97,7 +97,7 @@ export default class MusicCreate extends Component {
   render() {
     return (
       <div>
-        <h3>Ajouter une musique</h3>
+        <h3>{this.props.title}</h3>
         <form onSubmit={this.onSubmit}>
           <div className="form-group">
             <label>Nom: </label>
